cfb59c34c48a78f6e345589f743d97cf7b9aca61
Alberto <zonanabeto@gmail.com>
2019-10-27 20:47:23 -0600

    Listo modulo 14

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -58,8 +58,27 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-  const stores = await Store.find();
-  res.render('stores', {title:'Stores', stores: stores} );
+  const page = req.params.page || 1;
+  const limit = 6;
+  const skip = (page * limit) - limit;
+
+  const storesPromise =  Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({created: 'desc'});
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+  if(!stores.length && skip){
+    req.flash('info',  `Hey you asked for page ${page}. But that doesn't exist. So I put you on page ${pages}.`);
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+  res.render('stores', {title:'Stores', stores, pages, count, page} );
 }
 
 const confirOwner = (store, user) => {
